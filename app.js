29c89ce61d392fc6fd83fc8502e5c28f8a2b4ade
Carlos Queiroz <chqueiroz2002@gmail.com>
2025-01-27 10:10:22 -0300

    adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let inputAmigo = document.getElementById("amigo");
+
+function checkInputAmigo() {
+    const checkInput = inputAmigo.value;
+
+    if (checkInput === "") {
+        alert("Por favor, insira um nome.");
+    }
+    else {
+        amigos.push(checkInput)
+        limparCampo();
+    }
+}
+
+function adicionarAmigo() {
+    checkInputAmigo();
+}
+
+function limparCampo() {
+    inputAmigo.value = '';
+}
+
